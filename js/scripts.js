942adc9e1ad0afc60592bbb7ec3a49434a8b107f
GiuseppeF11 <faillagiuseppe13@gmail.com>
2023-12-22 15:37:26 +0100

    Aggiunti Bonus

--- js/scripts.js
+++ js/scripts.js
@@ -3,11 +3,17 @@
 console.log('Template html css js')
 
 const { createApp } = Vue;
+const imgCat = 'https://www.idlememe.com/wp-content/uploads/2021/12/merry-christmas-meme-idlememe-6.jpg'
+const imgTree = 'https://png.pngtree.com/png-clipart/20201208/original/pngtree-sparkling-christmas-tree-merry-png-image_5605495.jpg'
 
 createApp({
     data() {
         return {
             text: 'BUON NATALE!',
+            imgCat: imgCat,
+            imgTree: imgTree,
+            classes: 'rounded-5',
+            bg: 'bg-danger text-center py-3',
         }
     },
 }).mount('body');
