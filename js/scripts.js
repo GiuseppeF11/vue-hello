45c6d3a0143170659ba7156487ca5377f31bbfad
GiuseppeF11 <faillagiuseppe13@gmail.com>
2023-12-22 14:53:10 +0100

    Aggiunto Vue e stampati auguri su HTML

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,13 @@
 // Test funzionamento
 
 console.log('Template html css js')
+
+const { createApp } = Vue;
+
+createApp({
+    data() {
+        return {
+            text: 'BUON NATALE!',
+        }
+    },
+}).mount('body');
